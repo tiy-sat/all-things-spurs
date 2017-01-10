52cb21fcb7900a832f82a82f4b34fa807cd30a47
JavachipMom <Nicolesp4ever@gmail.com>
2017-01-10 14:35:59 -0600

    Finished my task for 3 blog post

--- js/main.js
+++ js/main.js
@@ -6,7 +6,21 @@ export default React.createClass({
   render () {
     return (
       <div>
-        <p>hefhksdfdjslkfj ds jsdfsdjkfkjdsjfsjk hjkv hdsjkhkjds jks</p>
+        <article>
+          <h3> Spurs Blog 1 </h3>
+          <img src="" alt=""/>
+          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </article>
+        <article>
+          <h3> Spurs Blog 2 </h3>
+          <img src="" alt=""/>
+          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </article>
+        <article>
+          <h3> Spurs Blog 3 </h3>
+          <img src="" alt=""/>
+          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </article>
       </div>
     )
   }
