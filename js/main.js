ef5685b3bf02eb5a0e6c75afc02f798b02ee8d15
JavachipMom <Nicolesp4ever@gmail.com>
2017-01-10 14:59:54 -0600

    Redid my 3 blog post changes

--- js/main.js
+++ js/main.js
@@ -7,19 +7,61 @@ export default React.createClass({
     return (
       <div>
         <article>
-          <h3> Spurs Blog 1 </h3>
-          <img src="" alt=""/>
-          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h1> San Antonio vs. Charlotte, Final Score: Bench sparks Spurs to 102-85 victory over Hornets </h1>
+          <img src="./styles/spurspic1.jpg" alt="spurs picture"/>
+          <p> The Spurs didn't have many shots fall for them in the first half against the Hornets, but a third quarter run led by the reserves, namely Davis Bertans, pushed the home team ahead, and the fourth quarter turned it into another laugher as San Antonio outscored Charlotte by 20 after intermission.
+
+The Spurs opened the game hitting their first three shots on the way to a 7-2 lead before the Hornets went on a little 10-4 run of their own to take a 12-11 lead midway through the first quarter. After a timeout, Patty Mills scored 8 points, and Davis Bertans (who got some early run with David Lee missing his first game of the season with a knee contusion) made his presence felt with a high-flying dunk over Roy Hibbert. The Hornets were led by 7 points from Kemba Walker, and the score was tied 24-24 at the end of the first quarter.
+
+The Spurs were a little sloppy to start the second quarter, but two threes from Kawhi Leonard and one from Danny Green helped the Spurs take a 35-33 lead five minutes into the quarter. There wasn’t much ball-movement from either team until the final few minutes of the half. Both teams traded baskets before Walker banked in a three on a potential four-point play. He missed the ensuing free-throw, and the Hornets ended a rather uninteresting half of basketball up 50-47. The Spurs hit 7 of 20 from three in the half but shot less than 39% overall.
+
+The Spurs continued to receive but miss open shots to start the third quarter, and fortunately for them so did Charlotte. A three from Pau Gasol and a couple of FT’s helped the Spurs tie things up at 54 a piece before Pop called a timeout. Both teams continued to shoot poorly until Davis Bertans completed a four-point play and hit another three on consecutive plays to get the Spurs up 68-60 with two minutes left. It was just the spark the Spurs needed, and they entered the final frame with a 73-64 lead.
+
+Bertans scored the Spurs’ first five points of the fourth quarter, Dewayne Dedmon got an alley-oop, and the lead was quickly stretched to 80-66. A Leonard block of a Hibbert dunk led to a three from Jonathon Simmons, and the route seemed to be on...until Bertans took a breather and the Hornets cut what was an 18-point lead down to 12. Bertans promptly returned and drew three more free throws. Another Davis three from a few feet behind the line got the lead back to 18 with 4:11 left, and then the route really was on. Bertans finished with a career high 21 points, and the Spurs won 102-85.
+
+Check back later for more on this one. The Spurs return to action on Tuesday to take on the Milwaukee Bucks. Be sure to check out Spurs Live before and after that game to see Matt Bonner’s studio debut!</p>
         </article>
         <article>
-          <h3> Spurs Blog 2 </h3>
-          <img src="" alt=""/>
-          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h1> Why the Spurs are so good this season </h1>
+          <img src="./styles/tonyparker.jpeg" alt="tony parker"/>
+          <p> This may seem ironic, considering the Spurs’ tough loss at Atlanta, but I feel more optimistic about them than I have all season. About ten different things had to happen for them to lose to the Hawks on New Year’s Day. They ran into a superhuman performance by Paul Millsap, and a career night from Tim Hardaway Jr. Kawhi Leonard was a shell of himself, still weak from a bout with a stomach bug. They got virtually nothing from Jonathon Simmons, Kyle Anderson and Dewayne Dedmon. An Atlanta squad who usually shoots threes very poorly (33.1 percent) just happened to make 14-of-28 of them against the Spurs. These things happen, and hopefully you don’t run into them in a playoff game.
+
+What is encouraging is not only the Spurs’ glossy 28-7 record but more so that the eye test and the numbers are starting to align with the standings, when that wasn’t the case earlier in the season, when the team was winning some coin-flip games more because of Leonard and the bench than anything else. LaMarcus Aldridge, Pau Gasol, Tony Parker, Danny Green and Manu Ginobili are all playing their best ball of the season these past two weeks and gradually the team’s scoring differential is growing.
+
+So, the question before us: Why are the 2016-17 Spurs so good?
+
+The easy answer is to say offense, because it just so happens that the Spurs’ Offensive Rating of 109.9 (per NBA.com’s Stats Index) is the best in franchise history. But offensive efficiency is up across the league, to the point where the Spurs only rank fifth in the NBA. That 109.9 would’ve tied for second last year, and led the league in each of the past two seasons before that. Conversely, their Defensive Rating of 101.6 looks mediocre, it’d have ranked eighth last year and 12th two seasons ago, but this season it’s fourth.
+
+However, even in a league that’s growing ever more homogenized, with everyone firing away from outside and learning to eschew long twos, the Spurs are still finding a way to stand out. What specifically are they doing well?
+
+For starters, they’re shooting threes with historic accuracy, nailing 40.7 percent of their attempts. Last season’s Golden State Warriors canned 41.6 percent, Alvin Gentry’s Phoenix Suns of 2009-10 hit 41.2, and our all-time champions, believe it or not, had a Curry on the roster, only it was Stephen’s father Dell on the 1996-97 Charlotte Hornets, who sank an incredible 42.8 percent of their threes as a team.
+
+This year’s Spurs are on pace for around 736 makes. We’d all like for them to take more, but they’re still making nine per game, which is on pace for best in franchise history. They’ve never made more than 8.5 per game before, and the last time they did that, they won a championship (and upped it to 8.8 per game on 40.9 percent in the playoffs).
+
+Incidentally, this edition of the Spurs is the first since 2000-01 that has the potential to boast as many as six dudes who finish the season with at least 20 dunks in Leonard, Aldridge, Gasol, Simmons, Dedmon and David Lee. That may not sound overly impressive, but the Spurs haven’t exactly been known for their aerial acrobatics during the Gregg Popovich Era. For example, the 2008-09 squad for example might as well have worn short shorts and mandated that their games only be televised in black-and-white. They had 75 dunks —as a team— for the whole season. Malik Hairston finished third on the team... with 8. They had a journeyman center named Pops Mensah-Bonsu who threw down like two jams in three games and he was promptly waived (...for showboating?). I hated the 2008-09 season.
+
+(By the way, I dare anyone to come up with all six Spurs on the 20 dunk club from that ‘01 team without looking it up.)
+
+It’s probably not a coincidence then that the Spurs are getting to the free-throw line more than any season since 2010-11. While 22.6 attempts per game doesn’t sound like a lot —because it’s not, still just 21st in the league— that’s a quantum leap for a franchise typically at or near the bottom of the rankings in this category. What’s uncanny is how accurate they’ve been at the line, far and away atop the NBA at 82.3 percent. For a reference point, the highest figure I found since 1996-97 was the 2012-13 Oklahoma City Thunder at 82.8 percent.
+
+What that accuracy means is that while they’re only 21st in attempts, they’re actually 14th in makes, knocking down 18.6 freebies per game. The last time a Spurs team made that many at the charity stripe, you had to go back to the 2004-05 season, with Tim Duncan close to his prime and Ginobili and Parker just entering theirs.
+
+Their ability to make threes and finish at the rim has the Spurs tied-for-second in overall field goal percentage, despite their love affair for mid-range twos (though they have solid shooters taking the majority of those, too). The team ethos for moving the ball and looking for great shots instead of settling for good ones has enabled the Spurs to be in the top-five in field goal percentage for years. Their last season outside the top-five was 2009-2010 (where they ranked sixth) and you have to go back to 2007-08 for their last finish outside the top-10.
+
+Add it all up and the Spurs’ Effective Field Goal Percentage (53.1, fourth in the league) is as good as it’s ever been, behind the championship “Beautiful Game” 2013-14 edition, that finished at 53.8 percent. They’re right there, even with Ginobili, Parker and Gasol winding down, and much of the bench consisting of newcomers or unproven youngsters. That level of chemistry has been impressive.
+
+Defensively the Spurs haven’t been dominant in any area but one, but they aren’t bad at anything either. They control pace and keep the number of possessions down. They take care of their boards at an acceptable level. They’re in the top-10 in both steals (eighth, with 8.1 per game) and blocks (fourth, with 5.7) and they do those things without fouling much, which is pretty counterintuitive.
+
+Where they’ve been dominant, is in the area that’s most important of all, and that’s three-point defense. By hook or crook, they’re third, at 34.1 percent. Teams try a fair amount against them, but not many have had Hawks-like success.
+
+So what all that efficiency boils down to is this. The Spurs make a couple more buckets per game than their opponents, a .5 more threes per night, and about two-and-a-half more free-throws. Those fractions may seem marginal, just a point here and a point there, but those fractions add up and often make the difference between a win and a loss or a close win and a comfortable one.</p>
         </article>
         <article>
-          <h3> Spurs Blog 3 </h3>
-          <img src="" alt=""/>
-          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h1> Power Rankings, Week 12: Spurs regaining some street cred </h1>
+          <img src="./styles/coachpop.jpg" alt="coach pop"/>
+          <p> Remember a few weeks ago when everyone was worried about Kawhi Leonard being frozen out on the defensive end and lethargic offense from basically everyone except Leonard and the bench? Yeah, times have changed. The starters are beginning to look like a cohesive unit that’s been together for more than a couple of months, and the defense has gone from a decent-but-not-championship-caliber unit at the beginning of the season back to the terror the Spurs have always prided themselves on being.
+
+Sure, the current stretch of schedule isn’t exactly the toughest, but back in November the Spurs were caught taking their opponents for granted and either losing or having to rally back late to beat lesser teams. That is no longer the case, as the Spurs have held a 20 point lead in seven of their last eight games and are leaving no doubt who the better team is. They look like a team that is having fun out there, and that is a very good sign as the second half of the schedule draws near.</p>
         </article>
       </div>
     )
