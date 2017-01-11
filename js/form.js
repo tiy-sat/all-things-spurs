cff32c9ee316f5ff4531cea5c14a3af07554e413
David Rangel <rangelography@gmail.com>
2017-01-10 22:20:34 -0600

    updated form.js to set inital state for input values

--- js/form.js
+++ js/form.js
@@ -2,6 +2,18 @@ import React from 'react';
 
 
 export default React.createClass({
+  getInitialState: function() {
+    return {
+      userNameInput: '',
+      dateOfPost: '',
+      imageUrlInput: '',
+      blogTextInput: '',
+    };
+  },
+
+  submitButtonClick (e){
+    e.preventDefault();
+  },
 
   render () {
     return (
@@ -12,6 +24,7 @@ export default React.createClass({
           <textarea className="blogTextInput"type="text" name="" value="" placeholder="Enter blog post here"></textarea>
           <input className="submitPost" type="submit" name="" value="Submit Post"/>
         </form>
-    )
+          <input className="submitPost" type="submit" name="" value="Submit Post" onClick={ this.submitButtonClick }/>
+          </form>
   }
 })
