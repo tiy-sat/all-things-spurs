2f18a9e996bd36e87048845db05cec725e0860e1
JavachipMom <Nicolesp4ever@gmail.com>
2017-01-11 16:05:38 -0600

    Found my merge conflicts and created the wysiwyg

--- js/form.js
+++ js/form.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Editable from 'react-wysiwyg-editor'
 
 export default React.createClass({
   getInitialState: function() {
@@ -8,6 +8,7 @@ export default React.createClass({
       dateOfPost: '',
       imageUrlInput: '',
       blogTextInput: '',
+      content: ''
     };
   },
 
@@ -60,6 +61,7 @@ export default React.createClass({
           <input className="userNameInput" type="text" name="userName" value={ this.state.userNameInput } onChange={ this.updateUserNameInput } placeholder="Enter User Name"/>
           <input className="postDateInput" type="date" name="postDate" value={ this.state.dateOfPost } onChange={ this.updateDateOfPost }/>
           <input className="imageUrlInput" type="url" name="imageLink" value={ this.state.imageUrlInput } onChange={ this.updateImageUrlInput } placeholder="Enter Image URL"/>
+          <Editable className="wysiwygButtons" content={ this.state.content } onChange={ this.updateContentInput }/>
           <textarea className="blogTextInput"type="text" name="" value={ this.state.blogTextInput } onChange={ this.updateBlogTextInput } placeholder="Enter blog post here"></textarea>
           <input className="submitPost" type="submit" name="" value="Submit Post" onClick={ this.submitButtonClick }/>
         </form>
