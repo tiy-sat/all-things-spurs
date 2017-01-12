2b1442cccd74931024570c00a74655466fcf4460
David Rangel <rangelography@gmail.com>
2017-01-12 17:41:14 -0600

    got posts lists to hide and modal button to open them.

--- js/blogPost.js
+++ js/blogPost.js
@@ -19,15 +19,16 @@ export default React.createClass({
 
   render (){
     return (
+      <div>
+      <Modal updatePostVisible={this.updatePostVisible} isModalOpen={this.getModalOpenState} closeModal={this.makeModalCloseState} postData={this.props.postData}/>
       <article className="blog">
-        <div className="top">
+        <div className="bottom" ref="unhide">
+        <div className="top hide">
           <h2 className="postTitle">{this.props.titleInput}</h2>
           <img className="accordion_img" src={this.props.imageUrlInput}/>
           <h2 className="userName">{this.props.userNameInput}</h2>
           <h3 className="postDate">{this.props.dateOfPost}</h3>
         </div>
-        <Modal updatePostVisible={this.updatePostVisible} isModalOpen={this.getModalOpenState} closeModal={this.makeModalCloseState}postData={this.props.postData}/>
-        <div className="bottom" ref="unhide">
           <div className="spurAndButtons">
             <img src="http://i.cdn.turner.com/nba/nba/.element/media/2.0/teamsites/spurs/Schad/img/PostgameTeams/SAS.png"
             alt="spur"
@@ -37,9 +38,10 @@ export default React.createClass({
             alt="spur"
             className="lilspur"/>
           </div>
-          <p className="blogText">{this.props.blogTextInput}</p>
+          <p className="blogText hide">{this.props.blogTextInput}</p>
         </div>
       </article>
+      </div>
     )
   }
 })
--- js/modal.js
+++ js/modal.js
@@ -5,9 +5,13 @@ export default React.createClass({
   render() {
     return (
       <div className={this.props.isModalOpen() ? "visible" : "hidden"}>
-        <div>
-          <h2>{this.props.postData.title}</h2>
-      <p><button onClick={() => this.props.closeModal()}>Close</button></p>
+        <div className="top">
+          <h2 className="postTitle">{this.props.postData.title}</h2>
+          <img className="accordion_img" src={this.props.postData.image}/>
+          <h2 className="userName">{this.props.postData.name}</h2>
+          <h3 className="postDate">{this.props.postData.date}</h3>
+          <p className="blogText">{this.props.postData.text}</p>
+      <p><button className="clickhere_button" onClick={() => this.props.closeModal()}>Close</button></p>
         </div>
       </div>
     )
