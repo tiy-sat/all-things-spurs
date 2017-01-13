3a3c17153d7764cde69140933937f5f268e7fa99
David Rangel <rangelography@gmail.com>
2017-01-12 18:36:18 -0600

    got modals working sort of. Need further styling

--- js/modal.js
+++ js/modal.js
@@ -4,14 +4,16 @@ export default React.createClass({
 
   render() {
     return (
-      <div className={this.props.isModalOpen() ? "visible" : "hidden"}>
-        <div className="top">
-          <h2 className="postTitle">{this.props.postData.title}</h2>
-          <img className="accordion_img" src={this.props.postData.image}/>
-          <h2 className="userName">{this.props.postData.name}</h2>
-          <h3 className="postDate">{this.props.postData.date}</h3>
-          <p className="blogText">{this.props.postData.text}</p>
-      <p><button className="clickhere_button" onClick={() => this.props.closeModal()}>Close</button></p>
+      <div className="modalPosition">
+        <div className={this.props.isModalOpen() ? "visible" : "hidden"}>
+          <div className="top">
+            <h2 className="postTitle">{this.props.postData.title}</h2>
+            <img className="accordion_img" src={this.props.postData.image}/>
+            <h2 className="userName">{this.props.postData.name}</h2>
+            <h3 className="postDate">{this.props.postData.date}</h3>
+            <p className="blogText">{this.props.postData.text}</p>
+        <p><button className="clickhere_button" onClick={() => this.props.closeModal()}>Close</button></p>
+          </div>
         </div>
       </div>
     )
