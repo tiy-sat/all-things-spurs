d4a94fba57e9c9ec9fdef88ba2df04056628a33d
terika <terika.collins@gmail.com>
2017-01-13 14:39:41 -0600

    corrected syntax errors for engagementvids-test.js. smoke test successfully passed

--- __tests__/engagementvids-test.js
+++ __tests__/engagementvids-test.js
@@ -2,16 +2,17 @@ import React from 'react'
 import TestUtils from 'react-addons-test-utils'
 import Engagementvids from '../js/engagementvids'
 
-describe(engagementvids, () => {
+describe("engagementvids", () => {
   let video_sectionComponent = {}
 
   beforeEach(() =>{
     video_sectionComponent = TestUtils.renderIntoDocument(<Engagementvids />)
   })
   it("Should have a video", () => {
-    const video_sectionElement = TestUtils.findRenderedDomComponentWithClass(
+    const video_sectionElement = TestUtils.findRenderedDOMComponentWithClass(
       video_sectionComponent,
       "engagementvids"
     )
+    expect(video_sectionElement).toBeDefined()
   })
 })
