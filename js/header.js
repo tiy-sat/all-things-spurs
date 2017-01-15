5ade1b789d9376cd261cdc84f5a753d9d8ab891f
David Rangel <rangelography@gmail.com>
2017-01-15 12:41:35 -0600

    transmuted some syting to vw instead of px

--- js/header.js
+++ js/header.js
@@ -14,15 +14,15 @@ export default React.createClass({
         <img src="http://hdwpp.net/a/1920x1080/g/764/s3/spurs-theme-vl11.jpg"
         alt="Spurs Logo"
         className="logo"/>
-
-        <img src="http://i.cdn.turner.com/nba/nba/.element/media/2.0/teamsites/spurs/Schad/img/PostgameTeams/SAS.png"
-        alt="spur"
-        className="spur2"/>
         <div className="div_signin">
           <h2>
             <a href="#" className="signin">SIGN IN</a>
           </h2>
         </div>
+        <img src="http://i.cdn.turner.com/nba/nba/.element/media/2.0/teamsites/spurs/Schad/img/PostgameTeams/SAS.png"
+        alt="spur"
+        className="spur2"/>
+
       </div>
       </header>
     )
